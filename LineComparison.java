6a4e1d534ce74759d4361e03ccd994a136b66f48
nikita1803 <rainikita595@gmail.com>
2021-05-13 00:20:54 +0530

    Added: Calculated the lines are equal or not using equal function

--- LineComparison.java
+++ LineComparison.java
@@ -1,17 +1,42 @@
 package lineComparison;
 import java.util.*;
-import java.lang.Math;
+import java.lang.*;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome in Line Comparison");
 		
 		Scanner sc = new Scanner(System.in);
+		
 		int x1= sc.nextInt();
 		int y1= sc.nextInt();
 		int x2= sc.nextInt();
 		int y2= sc.nextInt();
-		double length = 0;
-		length = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-		System.out.println(Math.sqrt(length));
+		
+		double length1 = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		System.out.println(Math.sqrt(length1));
+		
+		int x3= sc.nextInt();
+		int y3= sc.nextInt();
+		int x4= sc.nextInt();
+		int y4= sc.nextInt();
+		
+		
+		double length2 = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		System.out.println(Math.sqrt(length2));
+		Double obj1 = new Double(length1);
+        Double obj2 = new Double(length2);
+		
+		if(obj1.equals(obj2)){  
+			  
+            System.out.println("Lines are equal");  
+        }  
+        else  
+        {  
+            System.out.println("Lines are not equal");  
+        }  
+			
+		}
 	}
-}
+
+	
+
