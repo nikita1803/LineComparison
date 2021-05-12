9bbbed852ecb27d36dfc6ab6dc50ec011069ba77
nikita1803 <rainikita595@gmail.com>
2021-05-12 23:31:25 +0530

    Added: Calculated the length of the line

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,17 @@
 package lineComparison;
-
+import java.util.*;
+import java.lang.Math;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome in Line Comparison");
+		
+		Scanner sc = new Scanner(System.in);
+		int x1= sc.nextInt();
+		int y1= sc.nextInt();
+		int x2= sc.nextInt();
+		int y2= sc.nextInt();
+		double length = 0;
+		length = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		System.out.println(Math.sqrt(length));
 	}
 }
